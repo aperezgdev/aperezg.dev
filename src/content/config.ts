39e38470fe932785d97aaceadad8417688b8194a
Alex Perez <apereazgdev@gmail.com>
2025-09-12 23:55:22 +0200

    initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from "astro:content";
+
+const posts = defineCollection({
+  schema: z.object({
+    id: z.string(),
+    title: z.string(),
+    description: z.string(),
+    publishedDate: z.string(),
+    estimatedTimeToRead: z.string(),
+  }),
+});
+
+export const collections = { blog: posts };
