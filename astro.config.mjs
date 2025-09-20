bee09353f0589ead1c4350dfa7528d96c49033c5
Alex Perez Garcia <aperezgdev@gmail.com>
2025-09-20 12:43:57 +0200

    add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,22 @@
 import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "es",
+        locales: {
+          es: "es",
+          en: "en",
+        },
+      },
+    }),
+  ],
   i18n: {
     defaultLocale: "es",
     locales: ["en", "es"],
